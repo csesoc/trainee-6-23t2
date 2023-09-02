13ec3b074d7b0306c0bdd6646a848a6bd642597e
justin <justinlew66@gmail.com>
2023-09-02 21:56:06 +1000

    Authentication server and some api functions, still need to do refresh tokens

---
+++ src/api/api.js
@@ -0,0 +1,13 @@
+export async function fetchProfile(token) {
+    const result = await fetch('https://api.spotify.com/v1/me', {
+        method: "GET", headers: {'Authorization': 'Bearer ' + token}
+    });
+    return await result.json();
+}
+
+export async function fetchUserPlaylists(token) {
+    const result = await fetch('https://api.spotify.com/v1/me/playlists', {
+        method: "GET", headers: {'Authorization': 'Bearer ' + token}
+    });
+    return await result.json();
+}
