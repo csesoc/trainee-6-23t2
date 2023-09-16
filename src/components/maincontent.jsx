2042c91a68ee90decf75ca12a5f06034547eac67
someretical <29365738+someretical@users.noreply.github.com>
2023-09-17 02:08:16 +1000

    made main list all button

--- src/components/maincontent.jsx
+++ src/components/maincontent.jsx
@@ -1,94 +1,130 @@
-import { useState, useEffect } from 'react'
-import './main.css'
+import "./main.css";
 
-export default function Maincontent({selectedItemData, setPlaybackItem, selectedType}) {
+import { useEffect, useState } from "react";
 
-  
-    return (
-    <div className='main-list-wrap'>
-        {selectedItemData.items ? 
-        <table className='main-list' style={{width:"100%"}}>
-            <thead >
-                <tr>
-                    <th>#</th>
-                    <th>Title</th>
-                    {selectedType === 'playlist' ? <th>Album</th>  : null}
-                    {selectedType === 'playlist' ? <th>Date Added</th> : null}
-                    <th>Length</th>
-                </tr>
-            </thead>
-            <tbody>
-                {selectedType === 'playlist' ? 
-                selectedItemData.items.map((item, index) => <Playlistitem key={index} index={index} item={item} />) 
-                :
-                selectedItemData.items.map((item, index) => <Albumitem key={index} index={index} item={item} />)}
-            </tbody>
+export default function MainContent({
+  selectedItemData,
+  setPlaybackItem,
+  selectedType,
+}) {
+  return (
+    <div className="main-list-wrap">
+      {selectedItemData.items ? (
+        <table className="main-list" style={{ width: "100%" }}>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Title</th>
+              {selectedType === "playlist" ? <th>Album</th> : null}
+              {selectedType === "playlist" ? <th>Date Added</th> : null}
+              <th>Length</th>
+            </tr>
+          </thead>
+          <tbody>
+            {selectedType === "playlist"
+              ? selectedItemData.items.map((item, index) => (
+                  <PlaylistItem key={index} index={index} item={item} />
+                ))
+              : selectedItemData.items.map((item, index) => (
+                  <AlbumItem key={index} index={index} item={item} />
+                ))}
+          </tbody>
         </table>
-        : null}
+      ) : null}
     </div>
-  )
+  );
 }
 
-function Albumitem({item, index}) {
-    return (
-        <tr>  
-            <td width="5%">{index + 1}</td>
-            <td className='main-list-title'>
-                <div className='main-list-album-text'>
+function AlbumItem({ item, index }) {
+  return (
+    <tr>
+      <td colSpan="5">
+        <button className="main-list-button" >
+          <table className="main-list" style={{ width: "100%" }}>
+            <tbody>
+              <tr>
+                <td width="5%">{index + 1}</td>
+                <td className="main-list-title">
+                  <div className="main-list-album-text">
                     <div>
-                        <b>{item.name}</b>
+                      <b>{item.name}</b>
                     </div>
-                    <div style={{color:"silver"}}>
-                        {item.artists[0].name}
+                    <div style={{ color: "silver" }}>
+                      {item.artists[0].name}
                     </div>
-                </div>
-            </td>
-            <td width="15%">{ms_to_display(item.duration_ms)}</td>
-        </tr>
-    )
+                  </div>
+                </td>
+                <td width="15%">{ms_to_display(item.duration_ms)}</td>
+              </tr>
+            </tbody>
+          </table>
+        </button>
+      </td>
+    </tr>
+  );
 }
 
-function Playlistitem({item, index}) {
-    return (
-        <tr>  
-            <td width="5%">{index + 1}</td>
-            <td className='main-list-title'>
-                <img src={item.track.album.images[item.track.album.images.length - 1].url} 
-                        alt=":(" width="50" height="50"></img>
-                <div className='main-list-text'>
+function PlaylistItem({ item, index }) {
+  return (
+    <tr>
+      <td colSpan="5">
+        <button className="main-list-button">
+          <table className="main-list" style={{ width: "100%" }}>
+            <tbody>
+              <tr>
+                <td width="5%">{index + 1}</td>
+                <td className="main-list-title">
+                  <img
+                    src={
+                      item.track.album.images[
+                        item.track.album.images.length - 1
+                      ].url
+                    }
+                    alt=":("
+                    width="50"
+                    height="50"
+                  ></img>
+                  <div className="main-list-text">
                     <div>
-                        <b>{item.track.name}</b>
+                      <b>{item.track.name}</b>
                     </div>
-                    <div style={{color:"silver"}}>
-                        {item.track.artists[0].name}
+                    <div style={{ color: "silver" }}>
+                      {item.track.artists[0].name}
                     </div>
-                </div>
-            </td>
-            <td width="20%" className='main-list-name'>{item.track.album.name}</td>
-            <td width="15%">{date_to_display(item.added_at)}</td>
-            <td width="15%">{ms_to_display(item.track.duration_ms)}</td>
-        </tr>
-    )
+                  </div>
+                </td>
+                <td width="20%" className="main-list-name">
+                  {item.track.album.name}
+                </td>
+                <td width="15%">{date_to_display(item.added_at)}</td>
+                <td width="15%">{ms_to_display(item.track.duration_ms)}</td>
+              </tr>
+            </tbody>
+          </table>
+        </button>
+      </td>
+    </tr>
+  );
 }
 
 function date_to_display(date) {
-    // Convert date to Month /dd/yyyy
-    const dateObj = new Date(date);
-    const month = dateObj.toLocaleString('default', { month: 'short' });
-    const day = dateObj.getDate();
-    const year = dateObj.getFullYear();
+  // Convert date to Month /dd/yyyy
+  const dateObj = new Date(date);
+  const month = dateObj.toLocaleString("default", { month: "short" });
+  const day = dateObj.getDate();
+  const year = dateObj.getFullYear();
 
-    return `${month} ${day}, ${year}`;
+  return `${month} ${day}, ${year}`;
 }
 
 function ms_to_display(milliseconds) {
-    // Convert ms to minutes and seconds
-    const totalSeconds = Math.floor(milliseconds / 1000);
-    const minutes = Math.floor(totalSeconds / 60);
-    const seconds = totalSeconds % 60;
+  // Convert ms to minutes and seconds
+  const totalSeconds = Math.floor(milliseconds / 1000);
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
 
-    const formattedMinutes = minutes.toString().padStart(2, '0');
-    const formattedSeconds = seconds.toString().padStart(2, '0');
+  const formattedMinutes = minutes.toString().padStart(2, "0");
+  const formattedSeconds = seconds.toString().padStart(2, "0");
 
-    return `${formattedMinutes}:${formattedSeconds}`;
+  return `${formattedMinutes}:${formattedSeconds}`;
 }
